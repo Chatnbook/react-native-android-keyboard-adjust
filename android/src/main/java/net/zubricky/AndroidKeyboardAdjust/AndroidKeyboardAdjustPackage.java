604325f8ca8fdff51a7ff4ce43232a351481a26a
sertony <serkov_anton@mail.ru>
2017-10-24 18:59:06 +0300

    add createJSModules

--- android/src/main/java/net/zubricky/AndroidKeyboardAdjust/AndroidKeyboardAdjustPackage.java
+++ android/src/main/java/net/zubricky/AndroidKeyboardAdjust/AndroidKeyboardAdjustPackage.java
@@ -22,6 +22,11 @@ public class AndroidKeyboardAdjustPackage implements ReactPackage {
     }
 
     @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
         return Arrays.<ViewManager>asList();
     }
